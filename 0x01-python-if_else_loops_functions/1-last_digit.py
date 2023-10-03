ee19923fbcc8ff96b8c7d903d52daa94bda4506d
TKaburu <traceymwendwa@gmail.com>
2023-10-03 08:17:43 -0700

    still not getting the correct output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ld = number % 10
+ld = abs(number) % 10 #using modulas to find lst digit 
 if number < 0:
-    print(f"Last digit of {number:d} is {ld:d} and is ")
+    ld = -ld
+print(f"Last digit of {number:d} is {ld:d} and is ")
 if ld > 5:
     print("greater than 5")
-if ld == 0:
+elif ld == 0:
     print("0")
 else:
     print("less than 6 and not 0")
