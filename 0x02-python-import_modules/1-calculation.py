830d4c57208dbe79a59ed6e726c197c26a6568f7
TKaburu <traceymwendwa@gmail.com>
2023-10-05 13:05:12 -0700

    Fixed pycode style and added correct operators

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,12 +2,12 @@
 if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     a = 10
-    b =5
+    b = 5
     add_nums = add(a, b)
     sub_nums = sub(a, b)
     multi_nums = mul(a, b)
     div_nums = div(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, add_nums))
-    print("{:d} + {:d} = {:d}".format(a, b, sub_nums))
-    print("{:d} + {:d} = {:d}".format(a, b, multi_nums))
-    print("{:d} + {:d} = {:d}".format(a, b, div_nums))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_nums))
+    print("{:d} * {:d} = {:d}".format(a, b, multi_nums))
+    print("{:d} / {:d} = {:d}".format(a, b, div_nums))
