2d53fed22c0c77450b87da32acd2d57c9ad1a53c
TKaburu <traceymwendwa@gmail.com>
2023-10-09 08:03:13 -0700

    Find the multiples of 2 in  a list

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    res = []
+    for num in my_list:
+        if num % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+    return res
