487f389935aaa325626c402eafbd2e31f5c6011e
TKaburu <traceymwendwa@gmail.com>
2023-10-11 06:15:02 -0700

    Fixed pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    cpy_list = my_list[ : ]
+    cpy_list = my_list[:]
     for t in range(len(cpy_list)):
         if cpy_list[t] == search:
-           cpy_list[t] = replace
+            cpy_list[t] = replace
     return cpy_list
