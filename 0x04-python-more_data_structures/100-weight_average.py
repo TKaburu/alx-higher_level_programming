10e19f53dbf38a97d804278600d66525c6b73e34
TKaburu <traceymwendwa@gmail.com>
2023-10-11 09:41:19 -0700

    returns the averaged weight of integer tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_t = 0
+    weight = 0
+    for item in my_list:
+        sum_t = sum_t +(item[0] * item[1])
+        weight = weight + item[-1]
+    return sum_t / weight
