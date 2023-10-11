2f28a8e8f0d0edaed68ccfab3c0aa4b766d7f61c
TKaburu <traceymwendwa@gmail.com>
2023-10-11 09:43:12 -0700

    Fixed pycodestyle error

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,6 @@ def weight_average(my_list=[]):
     sum_t = 0
     weight = 0
     for item in my_list:
-        sum_t = sum_t +(item[0] * item[1])
+        sum_t = sum_t + (item[0] * item[1])
         weight = weight + item[-1]
     return sum_t / weight
