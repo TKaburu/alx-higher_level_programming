931a1d87442a309a6e0de439fc564d60a8d165ca
TKaburu <traceymwendwa@gmail.com>
2023-10-24 19:42:51 -0700

    Cla with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Define class square"""
+
+
+class square:
+    """ This square cla has a private attribute size"""
+    def __init__(self, size):
+        self.__size = size
