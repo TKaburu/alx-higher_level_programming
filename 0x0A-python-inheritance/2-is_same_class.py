f9146ad958c5f6bbf3f234bff5b348193c5b92ef
TKaburu <traceymwendwa@gmail.com>
2023-11-06 05:28:28 -0800

    Fixing requirement

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@
 
 def is_same_class(obj, a_class):
     """ Defined function """
-    return(type(obj) is a_class)
+    if type(obj) == a_class:
+        return (True)
+    else:
+        return (False)
