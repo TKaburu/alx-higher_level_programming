269b581bff862ec9be6604943faac5088407a50f
TKaburu <traceymwendwa@gmail.com>
2023-11-06 07:02:03 -0800

    Pycodestyle fixed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,6 +18,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """ Define a child class Rectangle"""
     def __init__(self, width, height):
