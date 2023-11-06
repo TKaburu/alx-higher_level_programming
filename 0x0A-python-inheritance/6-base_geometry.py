2606ecc5d1435774cddf1906cc6e4a0963a2f591
TKaburu <traceymwendwa@gmail.com>
2023-11-06 06:28:56 -0800

    Class with public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Define the class """
+
+
+class BaseGeometry:
+    """ Define the method """
+    def area(self):
+        """This is a public instance method"""
+        raise Exception("area() is not implemented")
