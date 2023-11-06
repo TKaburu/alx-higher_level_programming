e58882b0ea49608ea6133f243df6a8963641f1ac
TKaburu <traceymwendwa@gmail.com>
2023-11-06 07:00:02 -0800

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" Define the class """
+
+
+class BaseGeometry:
+    """ Define the method """
+    def area(self):
+        """This is a public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        A public instance that validate value
+        """
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ Define a child class Rectangle"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
