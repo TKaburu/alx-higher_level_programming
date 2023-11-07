ab22dc0a96dbabe7d40300a96d9eb0b90c6f2489
TKaburu <traceymwendwa@gmail.com>
2023-11-07 08:08:08 -0800

    Output fixes (added encoding='utf-8)

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,9 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ Inserts a line of text after each line containing a specific string"""
-    with open(filename) as file_content:
+    with open(filename, encoding='utf-8') as file_content:
         lines = file_content.readlines()
-    with open(filename, mode="w") as file_content:
+    with open(filename, mode="w", encoding='utf-8') as file_content:
         for line in lines:
             file_content.write(line)
             if search_string in line:
