61a1aab2ac05fabd655c6fd696c867afeadf7a2f
Tracey Kaburu <128824824+TKaburu@users.noreply.github.com>
2023-11-07 12:03:34 +0300

    Don't use the txt file directly but rather use filename

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 
 def read_file(filename=""):
     """ This function reads a text file """
-    with open("my_file_0.txt", encoding="utf-8") as file_content:
+    with open(filename, encoding="utf-8") as file_content:
         file_read = file_content.read().strip()
         print(file_read)
