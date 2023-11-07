0bb8c7e12092692e61294ebfa4d5d7b905436282
TKaburu <traceymwendwa@gmail.com>
2023-11-07 01:52:00 -0800

    Fixing output fail (replaced split() with end ='')

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@
 def read_file(filename=""):
     """ This function reads a text file """
     with open(filename, encoding="utf-8") as file_content:
-        file_read = file_content.read().strip()
-        print(file_read)
+        print(file_content.read(), end='')
