58332cb7f1f990fd1b20bf12b4957c9dee9be331
TKaburu <traceymwendwa@gmail.com>
2023-11-07 02:23:01 -0800

    JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" import json modul """
+
+import json
+
+def to_json_string(my_obj):
+    """ Returns JSON representation of an object (string)"""
+    return(json.dumps(my_obj))
