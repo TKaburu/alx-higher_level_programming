aec3b70ba6cafe0807ec083df090e553ddeb91a3
TKaburu <traceymwendwa@gmail.com>
2023-11-07 08:04:55 -0800

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Search and update """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Inserts a line of text after each line containing a specific string"""
+    with open(filename) as file_content:
+        lines = file_content.readlines()
+    with open(filename, mode="w") as file_content:
+        for line in lines:
+            file_content.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
