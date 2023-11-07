379f154a335980a36d2e2c09540d418f7e090a96
TKaburu <traceymwendwa@gmail.com>
2023-11-07 04:44:06 -0800

    Fixing output fail

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-""" Import Json module """
+""" import json """
 
 
 import json
 
 
 def load_from_json_file(filename):
-    """ This function  creates an Object from a “JSON file” """
-    with open(filename, encoding="utf-8") as file_content:
-        print(json.load(file_content))
+    """ creates an Object from a “JSON file """
+    with open(filename) as file_content:
+        return (json.load(file_content))
