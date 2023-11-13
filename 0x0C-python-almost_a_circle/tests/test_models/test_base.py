2598a9ee19edbf8fa2d889c8459f147c00d6b0c4
Tracey Kaburu <128824824+TKaburu@users.noreply.github.com>
2023-11-13 22:55:22 +0300

    Testing if I am on the right track

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,13 @@
+from models.base import Base
+import unittest
+
+
+class TestBase_(unittest.TestCase):
+    def test_once(self):
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_noargs(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
