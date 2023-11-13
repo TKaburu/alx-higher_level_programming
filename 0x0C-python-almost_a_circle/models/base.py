57388814bfecd15f08306bde72a92b14623bc58b
TKaburu <traceymwendwa@gmail.com>
2023-11-13 06:45:57 -0800

    Updated base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+""" import Json module """
+
+
+import json
+
 """ Define parent class Base """
 
 
@@ -16,3 +21,11 @@ class Base:
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ This method returns the JSON string representation of list_dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
