5084d85ae65d5a89f96cdfe99540743c745ab038
TKaburu <traceymwendwa@gmail.com>
2023-11-13 02:32:02 -0800

    args and kwargs for the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,27 @@ class Square(Rectangle):
         self.width = data
         self.height = data
 
+    def update(self, *args, **kwargs):
+        """ public method that sets attributes with
+        args and kwargs
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+ 
