b599536001e97cafa5794e03ffd724074662c293
Tracey Kaburu <128824824+TKaburu@users.noreply.github.com>
2023-11-14 01:28:35 +0300

    fixing code output

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -62,7 +62,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """ This method sets all attributes """
-        instance = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            instance = cls(1
+        
         instance.update(**dictionary)
-
         return (instance)
