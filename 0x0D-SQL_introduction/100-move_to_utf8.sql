223ecad5c7dbf15e657139f83d46cc7205472dbe
TKaburu <traceymwendwa@gmail.com>
2023-12-15 12:22:45 -0800

    Converted name field as well

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,3 +5,6 @@ CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table
 CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table
+MODIFY COLUMN name VARCHAR(256)
+CHARACTER SET utf8 COLLATE utf8_general_ci;
