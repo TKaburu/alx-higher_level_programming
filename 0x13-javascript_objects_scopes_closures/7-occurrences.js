771aa0cd1c5b7e9ced33d2587f409790acd227cf
TKaburu <traceymwendwa@gmail.com>
2024-01-10 23:51:06 +0300

    Feat: Function that returns number of occurence in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0; let i = 0;
+  while (i < list.length) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+    i++;
+  }
+  return count;
+};
