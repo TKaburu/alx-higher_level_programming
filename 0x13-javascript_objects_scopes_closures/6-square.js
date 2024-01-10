b15ef85680402380265103ab5b6ffee354f67cde
TKaburu <traceymwendwa@gmail.com>
2024-01-10 22:56:09 +0300

    Feat: charPrint(c) method instance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    // Trying to use while loop
+    let i = 0;
+    while (i < this.height) {
+      if (c === undefined) {
+        c = 'X';
+      }
+      console.log(c.repeat(this.width));
+      i++;
+    }
+  }
+};
