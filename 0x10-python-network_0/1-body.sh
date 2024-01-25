c6f30ceb0207ab636c092ad76f29705ebb6500f4
TKaburu <traceymwendwa@gmail.com>
2024-01-25 11:26:21 -0800

    Fix: Pycodestyle by removing whitespaces

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# A script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# A script that takes in a URL, sends a GET request to the URL
+# and displays the body of the response
 
-curl -sL $1 
+curl -sL "$1"
