87dab639b6c2cd040ae6e7dd762a8cf1a637e961
TKaburu <traceymwendwa@gmail.com>
2024-01-26 06:44:22 -0800

    Checking smthing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ from urllib import parse, request
 
 if __name__ == "__main__":
 
-    data = parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    data = parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
 
     with request.urlopen(sys.argv[1], data) as response:
         content = response.read().decode('utf-8')
