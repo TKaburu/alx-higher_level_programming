fd322b871d1d165d6719167a8375ecb200edeac0
TKaburu <traceymwendwa@gmail.com>
2024-02-28 23:07:25 +0300

    Feat: No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the <header> element
+    const header = document.querySelector('header');
+
+    header.style.color = '#FF0000';
+});
